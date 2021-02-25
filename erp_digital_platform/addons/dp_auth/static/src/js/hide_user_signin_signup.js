de1034317a45e32b70659441074bd256101cd0b7
Nguyễn Mạnh Cường - CMC Global DU9 <nmcuong@cmc.com.vn>
2021-02-25 17:45:05 +0700

    them icon wish list

--- erp_digital_platform/addons/dp_auth/static/src/js/hide_user_signin_signup.js
+++ erp_digital_platform/addons/dp_auth/static/src/js/hide_user_signin_signup.js
@@ -7,3 +7,36 @@ $(document).ready(function() {
         }
     }
 })
+
+function showPassword(obj, el) {
+    var obj = document.getElementById('password');
+    if (obj.type === "password") {
+        obj.type = "text";
+        el.className = 'fa fa-eye-slash password field-icon';
+    } else {
+        obj.type = "password";
+        el.className = 'fa fa-eye password field-icon';
+    }
+}
+
+function showPasswordReset(obj, el) {
+    var obj = document.getElementById('password');
+    if (obj.type === "password") {
+        obj.type = "text";
+        el.className = 'fa fa-eye-slash password field-icon';
+    } else {
+        obj.type = "password";
+        el.className = 'fa fa-eye password field-icon';
+    }
+}
+
+function showPasswordResetConfirm(obj, el) {
+    var obj = document.getElementById('confirm_password');
+    if (obj.type === "password") {
+        obj.type = "text";
+        el.className = 'fa fa-eye-slash confirm_password field-icon';
+    } else {
+        obj.type = "password";
+        el.className = 'fa fa-eye confirm_password field-icon';
+    }
+}
